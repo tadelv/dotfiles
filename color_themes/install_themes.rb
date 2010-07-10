756760e6b771a4f9f62bfb6588d3e2e2dab447c0
Vid Tadel <tadelv@gmail.com>
2010-07-10 13:11:30 +0200

    gimme xcode themez!

---
+++ color_themes/install_themes.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+home = ENV['HOME']
+
+themes_dir = home + "/Library/Application Support/Xcode/Color Themes"
+
+
+Dir.chdir File.dirname(__FILE__) do
+  src_dir = Dir.pwd.sub(home+ '/', '')
+  
+  Dir['*'].each do |theme|
+    next if theme == 'install_themes.rb'
+    target = File.join(themes_dir, theme)
+    system %[ln -vsf #{File.join(src_dir, theme)} \'#{target}\']
+  end
+end
+
+
+  
--- install.rb
+++ install.rb
@@ -8,7 +8,7 @@ Dir.chdir File.dirname(__FILE__) do
   dotfiles_dir = Dir.pwd.sub(home + '/', '')
   
   Dir['*'].each do |file|
-    next if file == 'install.rb'
+    next if file == 'install.rb' || file == 'color_themes'
     target_name = file == 'bin' ? file : ".#{file}"
     target = File.join(home, target_name)
     system %[ln -vsf #{File.join(dotfiles_dir, file)} #{target}]
