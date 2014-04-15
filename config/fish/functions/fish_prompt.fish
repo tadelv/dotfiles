a54d39e90f6258487ffb36f8125e9ae0cf1ca8fd
Vid Tadel <tadelv@gmail.com>
2014-04-15 09:50:31 +0200

    added time to prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -72,8 +72,12 @@ function fish_prompt --description 'Write out the prompt'
   if not set -q __fish_prompt_host
     set -g __fish_prompt_host (set_color $fish_color_host)
   end
+  
+  function get_time
+	  date +%H:%M:%S
+  end
 
-  echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+  echo -n -s (get_time) ' ' "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 end
 
 
