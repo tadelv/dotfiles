59e1e7e2cfde27783e068405f18b55ada6decb2b
Vid Tadel <tadelv@gmail.com>
2014-11-07 16:09:07 +0100

    change default editor to textmate

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
 
 . ~/.config/fish/functions.fish
 
-set -x EDITOR 'subl -w'
+set -x EDITOR 'mate -w'
 set -x PATH ~/bin /usr/local/bin /opt/iOSOpenDev/bin $PATH
 set -x iOSOpenDevPath /opt/iOSOpenDev
