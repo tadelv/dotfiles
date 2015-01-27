23382317d1c150417f7e9a6488ba853901475414
Vid Tadel <tadelv@gmail.com>
2015-01-27 11:12:25 +0100

    replace spaces with tabs and modify extract function to support additional arguments

--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -1,118 +1,119 @@
 # echo "Loading functions ..."
 
 function extract --description "Extract an archive"
-  if begin; not test -z $argv[1]; and test -f $argv[1]; end
-    switch $argv[1]
-      case '*.tar.bz2'
-        tar xjf $argv[1]
-      case '*.tar.gz'
-        tar xzf $argv[1]
-      case '*.bz2'
-        bunzip2 $argv[1]
-      case '*.rar'
-        rar x $argv[1]
-      case '*.gz'
-        gunzip $argv[1]
-      case '*.tar'
-        tar xf $argv[1]
-      case '*.tbz2'
-        tar xjf $argv[1]
-      case '*.tgz'
-        tar xzf $argv[1]
-      case '*.zip'
-        unzip $argv[1]
-      case '*.Z'
-        uncompress $argv[1]
-      case '*.7z'
-        7za x $argv[1]
-      case '*'
-        echo "'$argv[1]' cannot be extracted via extract()" ;;
-    end
-  else
-    echo "'$argv[1]' is not a valid file"
-  end
+	set archive_path $argv[1]
+	if begin; not test -z $archive_path; and test -f $archive_path; end
+		switch $archive_path
+			case '*.tar.bz2'
+				tar xjf $argv
+			case '*.tar.gz'
+				tar xzf $argv
+			case '*.bz2'
+				bunzip2 $argv
+			case '*.rar'
+				rar x $argv
+			case '*.gz'
+				gunzip $argv
+			case '*.tar'
+				tar xf $argv
+			case '*.tbz2'
+				tar xjf $argv
+			case '*.tgz'
+				tar xzf $argv
+			case '*.zip'
+				unzip $argv
+			case '*.Z'
+				uncompress $argv
+			case '*.7z'
+				7za x $argv
+			case '*'
+				echo "'$archive_path' cannot be extracted via extract()" ;;
+		end
+	else
+		echo "'$archive_path' is not a valid file"
+	end
 end
 
 function l -d "long list"
-  ls -la $argv
+	ls -la $argv
 end
 
 # shorthands
 
 function work -d "change to work directory"
-  cd ~/development/work
+	cd ~/development/work
 end
 
 function dev -d "development directory"
-  cd ~/development
+	cd ~/development
 end
 
 function scratch
-  dev; cd scratch
+	dev; cd scratch
 end
 
 function repos
-  dev; cd repos
+	dev; cd repos
 end
 
 function dot
-  repos; cd dotfiles
+	repos; cd dotfiles
 end
 
 function xc
-  open *.xcodeproj
+	open *.xcodeproj
 end
 
 # Helpers
 
 function is_git
-  git status >/dev/null ^&1
-  return $status
+	git status >/dev/null ^&1
+	return $status
 end
 
 # SVN stuff
 
 function sup
-  svn up $argv[1]
+	svn up $argv[1]
 end
 
 function sco
-  svn co $argv[1]
+	svn co $argv[1]
 end
 
 function com
-  if is_git
-    git commit $argv
-  else
-    svn commit $argv
-  end
+	if is_git
+		git commit $argv
+	else
+		svn commit $argv
+	end
 end
 
 function st
-  if is_git
-    git status $argv
-  else
-    svn status $argv
-  end
+	if is_git
+		git status $argv
+	else
+		svn status $argv
+	end
 end
 
 function vpn_up #"Connect home"
- osascript -e 'tell application "System Events" 
-       tell current location of network preferences
-	       set VPN to service "Home (PPTP)"
-	       if exists VPN then connect VPN
-           repeat while (current configuration of VPN is not connected)
-               delay 1
-           end repeat
-       end tell
-   end tell'
+	osascript -e 'tell application "System Events" 
+		tell current location of network preferences
+			set VPN to service "Home (PPTP)"
+			if exists VPN then connect VPN
+			repeat while (current configuration of VPN is not connected)
+				delay 1
+			end repeat
+		end tell
+	end tell'
 end
 
 function vpn_down #"Connect home"
- osascript -e 'tell application "System Events" 
-       tell current location of network preferences
-	       set VPN to service "Home (PPTP)"
-	       if exists VPN then disconnect VPN
-       end tell
-   end tell'
+	osascript -e 'tell application "System Events" 
+		tell current location of network preferences
+			set VPN to service "Home (PPTP)"
+			if exists VPN then disconnect VPN
+		end tell
+	end tell'
 end
