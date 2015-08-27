acc0278f080c3352f040820f25ff4e3f17f63a37
Vid Tadel <tadelv@gmail.com>
2015-08-27 10:03:35 +0200

    add /usr/local/sbin to fish path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,5 @@
 . ~/.config/fish/functions.fish
 
 set -x EDITOR 'mate -w'
-set -x PATH ~/bin /usr/local/bin /opt/iOSOpenDev/bin $PATH
+set -x PATH ~/bin /usr/local/bin /usr/local/sbin /opt/iOSOpenDev/bin $PATH
 set -x iOSOpenDevPath /opt/iOSOpenDev
