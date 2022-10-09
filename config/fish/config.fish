715cf56e8e9c305756d681fdac3a747fe0cb4f6a
Vid Tadel <tadelv@gmail.com>
2022-10-09 11:00:18 +0200

    brew ruby paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,13 @@
 # Fish config file
-fish_add_path /usr/local/opt/ruby/bin
+
+if test -d "/usr/local/opt/ruby/bin"
+	fish_add_path /usr/local/opt/ruby/bin
+end
+
+if test -d "/opt/homebrew/opt/ruby/bin"
+	fish_add_path /opt/homebrew/opt/ruby/bin
+end
+
 fish_add_path (gem environment gemhome)/bin
 
 . ~/.config/fish/functions.fish
--- zshrc
+++ zshrc
@@ -2,3 +2,8 @@ if [ -d "/usr/local/opt/ruby/bin" ]; then
   export PATH=/usr/local/opt/ruby/bin:$PATH
   export PATH=`gem environment gemdir`/bin:$PATH
 fi
+
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
