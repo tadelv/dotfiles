b494b17a8b0f43d25cb7d56c3247dc2fe3f8e237
Vid Tadel <tadelv@gmail.com>
2024-06-20 23:30:32 +0200

    more config, remove xcodebuild and trouble conflicting keymap

--- config/nvim/lua/vid/core/init.lua
+++ config/nvim/lua/vid/core/init.lua
@@ -1,2 +1,4 @@
 require("vid.core.options")
 require("vid.core.keymaps")
+require("vid.core.colorscheme")
+require("vid.core.ui")
--- config/nvim/lua/vid/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/vid/plugins/nvim-lspconfig.lua
@@ -1,35 +1,66 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = cmp_nvim_lsp.default_capabilities()
-    local opts = { noremap = true, silent = true }
-    local on_attach = function(_, bufnr)
-      opts.buffer = bufnr
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require('lspconfig')
+      local ui = require('vid.core.ui')
+      local borders = { border = ui.borders }
+      local lsp = vim.lsp
+      local handlers = {
+        ["textDocument/hover"] = lsp.with(lsp.handlers.hover, borders),
+        ["txtdocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, borders),
+      }
 
-      opts.desc = "Show line diagnostics"
-      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+     local servers = {
+       clangd = {},
+       sourcekit = {
+         root_dir = lspconfig.util.root_pattern(
+           '.git',
+           'Package.swift',
+           'compile_commands.json'
+         ),
+       },
+       lspconfig.rust_analyzer.setup {}
+     }
 
-      opts.desc = "Show documentation for what is under cursor"
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    end
+     for server, setup in pairs(servers) do
+       setup.handlers = handlers
+       lspconfig[server].setup(setup)
+     end
 
-    lspconfig["sourcekit"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    -- nice icons
+     vim.api.nvim_create_autocmd('LspAttach', {
+       desc = 'LSP Actions',
+       callback = function(args)
+         -- Once we've attached, configure the keybindings
+         local wk = require('which-key')
+         wk.register({
+           K = {"<cmd>lua vim.lsp.buf.hover()<cr>", "LSP hover info"},
+           gd = {"<cmd>lua vim.lsp.buf.definition()<cr>", "LSP go to definition"},
+           gD = {"<cmd>lua vim.lsp.buf.declaration()<cr>", "LSP go to declaration"},
+           gi = {"<cmd>lua vim.lsp.buf.implementation()<cr>", "LSP go to implementation"},
+           gr = {"<cmd>lua vim.lsp.buf.references()<cr>", "LSP list references"},
+           gs = {"<cmd>lua vim.lsp.buf.signature_help()<cr>", "LSP signature help"},
+           gn = {"<cmd>lua vim.lsp.buf.rename()<cr>", "LSP rename"},
+           ["[g"] = {"<cmd>lua vim.diagnostic.goto_prev()<cr>", "Go to previous diagnostic"},
+           ["g]"] = {"<cmd>lua vim.diagnostic.goto_next()<cr>", "Go to next diagnostic"},
+         }, {
+           mode = 'n',
+           -- buffer = true,
+           silent = true,
+         })
+       end,
+     })
+          -- nice icons
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
-  end,
+
+   end
+  }
 }
+
+
+
