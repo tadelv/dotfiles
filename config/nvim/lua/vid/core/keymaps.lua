c69ea287d16c4f8f08aaf245befe864bb8362159
Vid Tadel <tadelv@gmail.com>
2024-06-21 14:39:14 +0200

    dont use jk to escape insert mode

--- config/nvim/lua/vid/core/keymaps.lua
+++ config/nvim/lua/vid/core/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+-- keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
