a0559a3eb556eb63fb2beeb78bc151f11bd96eed
Vid Tadel <tadelv@gmail.com>
2024-06-23 01:17:33 +0200

    disable autoformat, to work better with snippets and autosave

--- config/nvim/lua/vid/plugins/formatting.lua
+++ config/nvim/lua/vid/plugins/formatting.lua
@@ -22,11 +22,11 @@ return {
         python = { "isort", "black" },
         swift = { "swiftformat" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   async = false,
+      --   timeout_ms = 1000,
+      -- },
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
