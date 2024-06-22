cdfc6b9a8fb01ff68ec0a445635af7a21a547c74
Vid Tadel <tadelv@gmail.com>
2024-06-22 15:12:05 +0200

    trouble wip

--- config/nvim/lua/vid/plugins/trouble.lua
+++ config/nvim/lua/vid/plugins/trouble.lua
@@ -1,10 +1,12 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  cmd = "Trouble",
+  opts = {},
 	keys = {
 		{ "<leader>Tx", "<cmd>Trouble<CR>", desc = "Open/close trouble list" },
 		{ "<leader>Tw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>Td", "<cmd>Trouble document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+		{ "<leader>Td", "<cmd>Trouble diagnostics toggle<CR>", desc = "Toggle Trouble diagnostics" },
 		{ "<leader>Tq", "<cmd>Trouble quickfix<CR>", desc = "Open trouble quickfix list" },
 		{ "<leader>Tl", "<cmd>Trouble loclist<CR>", desc = "Open trouble location list" },
 		{ "<leader>Tt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
