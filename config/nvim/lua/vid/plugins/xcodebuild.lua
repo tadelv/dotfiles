c4c455b1091ca0f1ffd8828eedff357ab7afc87e
Vid Tadel <tadelv@gmail.com>
2024-08-26 20:30:27 +0200

    update

--- config/nvim/lua/vid/plugins/xcodebuild.lua
+++ config/nvim/lua/vid/plugins/xcodebuild.lua
@@ -1,66 +1,70 @@
 local progress_handle
 
 return {
-	"wojciech-kulik/xcodebuild.nvim",
-	dependencies = {
-		"nvim-telescope/telescope.nvim",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("xcodebuild").setup({
-			show_build_progress_bar = false,
-			logs = {
-				auto_open_on_success_tests = false,
-				auto_open_on_failed_tests = false,
-				auto_open_on_success_build = false,
-				auto_open_on_failed_build = true,
-				auto_focus = false,
-				auto_close_on_app_launch = true,
-				only_summary = true,
-				notify = function(message, severity)
-					local fidget = require("fidget")
-					if progress_handle then
-						progress_handle.message = message
-						if not message:find("Loading") then
-							progress_handle:finish()
-							progress_handle = nil
-							if vim.trim(message) ~= "" then
-								fidget.notify(message, severity)
-							end
-						end
-					else
-						fidget.notify(message, severity)
-					end
-				end,
-				notify_progress = function(message)
-					local progress = require("fidget.progress")
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-tree.lua",
+  },
+  config = function()
+    require("xcodebuild").setup({
+      show_build_progress_bar = true,
+      restore_on_start = true,
+      auto_save = true,
+      logs = {
+        auto_open_on_success_tests = false,
+        auto_open_on_failed_tests = false,
+        auto_open_on_success_build = false,
+        auto_open_on_failed_build = true,
+        auto_focus = false,
+        auto_close_on_app_launch = true,
+        -- only_summary = true,
+        notify = function(message, severity)
+          local fidget = require("fidget")
+          if progress_handle then
+            progress_handle.message = message
+            if not message:find("Loading") then
+              progress_handle:finish()
+              progress_handle = nil
+              if vim.trim(message) ~= "" then
+                fidget.notify(message, severity)
+              end
+            end
+          else
+            fidget.notify(message, severity)
+          end
+        end,
+        notify_progress = function(message)
+          local progress = require("fidget.progress")
 
-					if progress_handle then
-						progress_handle.title = ""
-						progress_handle.message = message
-					else
-						progress_handle = progress.handle.create({
-							message = message,
-							lsp_client = { name = "xcodebuild.nvim" },
-						})
-					end
-				end,
-			},
-			code_coverage = {
-				enabled = true,
-			},
-			commands = {
-				extra_build_args = "-skipMacroValidation",
-			},
-			console_logs = {
-				enabled = true,
-			},
+          if progress_handle then
+            progress_handle.title = ""
+            progress_handle.message = message
+          else
+            progress_handle = progress.handle.create({
+              message = message,
+              lsp_client = { name = "xcodebuild.nvim" },
+            })
+          end
+        end,
+      },
+      code_coverage = {
+        enabled = true,
+      },
+      commands = {
+        extra_build_args = "-skipMacroValidation -parallelizeTargets",
+        extra_test_args = "-skipMacroValidation -parallelizeTargets",
+      },
+      console_logs = {
+        enabled = true,
+      },
       integrations = {
         xcode_build_server = {
           enabled = true,
-        }
-      }
-		})
+        },
+      },
+    })
 
     -- stylua: ignore start
     vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" })
@@ -78,7 +82,7 @@ return {
     vim.keymap.set("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
     vim.keymap.set("n", "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", { desc = "Show Code Coverage Report" })
     vim.keymap.set("n", "<leader>xe", "<cmd>XcodebuildTestExplorerToggle<cr>", { desc = "Toggle Test Explorer" })
-    vim.keymap.set("n", "<leader>xs", "<cmd>XcodebuildFailingSnapshots<cr>", { desc = "Show Failing Snapshots" })
+    -- vim.keymap.set("n", "<leader>xs", "<cmd>XcodebuildFailingSnapshots<cr>", { desc = "Show Failing Snapshots" })
 
     vim.keymap.set("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
     vim.keymap.set("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
@@ -86,5 +90,5 @@ return {
 
     vim.keymap.set("n", "<leader>xx", "<cmd>XcodebuildQuickfixLine<cr>", { desc = "Quickfix Line" })
     vim.keymap.set("n", "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", { desc = "Show Code Actions" })
-	end,
+  end,
 }
