01edf4aae68531574dd8ed816874f7edec66efdb
Vid Tadel <tadelv@gmail.com>
2024-12-10 22:13:40 +0100

    update

--- config/nvim/lua/vid/plugins/nvim-cmp.lua
+++ config/nvim/lua/vid/plugins/nvim-cmp.lua
@@ -70,7 +70,6 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "copilot" },
         { name = "nvim_lsp" },
         { name = "luasnip", }, -- snippets
         { name = "buffer" }, -- text within current buffer
