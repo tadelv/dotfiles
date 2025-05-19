b2a2141deab7065e5ef8aa9a2ccc20d4d66f2bbe
Vid Tadel <tadelv@gmail.com>
2025-05-19 21:01:39 +0200

    nvim show diagnostics on hover

--- config/nvim/lua/vid/core/options.lua
+++ config/nvim/lua/vid/core/options.lua
@@ -41,3 +41,10 @@ opt.splitright = true
 opt.splitbelow = true
 
 -- opt.iskeyword:append("-")
+
+-- Show errors and warnings in a floating window
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
+    end,
+})
