6d5b66dc39e2e6205d7ce8fc976bc7e575060e99
Vid Tadel <tadelv@gmail.com>
2025-10-20 14:34:32 +0200

    chadding

--- config/nvim/lua/config/commands.lua
+++
@@ -1,9 +0,0 @@
-local myf = require("my-functions")
-
--- My custom Commad to export md as PDF
-vim.api.nvim_create_user_command("PDFExport", myf.ExportPDF, { nargs = "*" })
-
--- simply and useless method to highlight a word
--- vim.api.nvim_create_user_command("HLWord", function(opts)
---   myf.HLHighlightWord(opts.args)
--- end, { nargs = 1 })
